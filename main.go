c1cdb042430da3597b678fbaba8e9cc555acc556
Rhys <lomirus@qq.com>
2021-01-22 17:26:47 +0800

    added register and login page links

--- main.go
+++ main.go
@@ -10,11 +10,18 @@ func main() {
     router := gin.Default()
     router.LoadHTMLGlob("./view/html/*")
     router.Static("statics","./view/statics")
+    passport := router.Group("passport")
     router.GET("/", func(c *gin.Context){
         c.HTML(http.StatusOK, "index.html", nil)
     })
     router.GET("/favicon.ico", func(c *gin.Context){
         c.File("./view/favicon.ico")
+    })
+    passport.GET("/register", func(c *gin.Context){
+        c.HTML(http.StatusOK, "register_pc.html", nil)
+    })
+    passport.GET("/login", func(c *gin.Context){
+        c.HTML(http.StatusOK, "login_pc.html", nil)
     })
     if err := router.Run(":8080"); err != nil {
         log.Fatal(err)
