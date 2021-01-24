0074486fe78ee6bcd7b33d90b82e43ba25ff44a4
Rhys <lomirus@qq.com>
2021-01-24 23:17:34 +0800

    finished space banner bottom tab

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "bilibili/controller"
     "bilibili/tool"
     "github.com/gin-gonic/gin"
+    "html/template"
     "log"
     "net/http"
 )
@@ -40,6 +41,27 @@ func main() {
         uid := c.Param("uid")
         c.HTML(http.StatusOK, "space.html", gin.H{
             "uid": uid,
+            "tab": template.HTML(`<div id="tab_home" class="now_tab">主页</div>
+            <div id="tab_moments">动态</div>
+            <div id="tab_post">投稿</div>`),
+        })
+    })
+    space.GET("/:uid/moments", func(c *gin.Context){
+        uid := c.Param("uid")
+        c.HTML(http.StatusOK, "space.html", gin.H{
+            "uid": uid,
+            "tab": template.HTML(`<div id="tab_home">主页</div>
+            <div id="tab_moments" class="now_tab">动态</div>
+            <div id="tab_post">投稿</div>`),
+        })
+    })
+    space.GET("/:uid/post", func(c *gin.Context){
+        uid := c.Param("uid")
+        c.HTML(http.StatusOK, "space.html", gin.H{
+            "uid": uid,
+            "tab": template.HTML(`<div id="tab_home">主页</div>
+            <div id="tab_moments">动态</div>
+            <div id="tab_post" class="now_tab">投稿</div>`),
         })
     })
 
---
+++ view/statics/js/space.js
@@ -0,0 +1,54 @@
+const tab_home = document.querySelector('#tab_home')
+const tab_moments = document.querySelector('#tab_moments')
+const tab_post = document.querySelector('#tab_post')
+const tab_underline = document.querySelector('#tab_underline')
+const uid = location.pathname.split('/')[2]
+let tab_underline_left = '15px'
+
+tab_home.addEventListener('mouseover', () => moveUnderline('15px'))
+tab_moments.addEventListener('mouseover', () => moveUnderline('96px'))
+tab_post.addEventListener('mouseover', () => moveUnderline('177px'))
+tab_home.addEventListener('mouseout', () => moveUnderline(tab_underline_left))
+tab_moments.addEventListener('mouseout', () => moveUnderline(tab_underline_left))
+tab_post.addEventListener('mouseout', () => moveUnderline(tab_underline_left))
+
+tab_home.addEventListener('click', function() {
+    history.pushState(null ,null ,`/space/${uid}`)
+    tab_home.setAttribute('class', 'now_tab')
+    tab_moments.removeAttribute('class')
+    tab_post.removeAttribute('class')
+    tab_underline_left = '15px'
+})
+
+tab_moments.addEventListener('click', function() {
+    history.pushState(null ,null ,`/space/${uid}/moments`)
+    tab_home.removeAttribute('class')
+    tab_moments.setAttribute('class', 'now_tab')
+    tab_post.removeAttribute('class')
+    tab_underline_left = '96px'
+})
+
+tab_post.addEventListener('click', function() {
+    history.pushState(null ,null ,`/space/${uid}/post`)
+    tab_home.removeAttribute('class')
+    tab_moments.removeAttribute('class')
+    tab_post.setAttribute('class', 'now_tab')
+    tab_underline_left = '177px'
+})
+
+function moveUnderline(left) {
+    tab_underline.style.left = left
+}
+
+function init(){
+    if(location.pathname.split('/').length === 4){
+        if(location.pathname.split('/')[3] === 'moments'){
+            tab_underline_left = '96px'
+        } else {
+            tab_underline_left = '177px'
+        }
+    }
+    moveUnderline(tab_underline_left)
+}
+
+init()
