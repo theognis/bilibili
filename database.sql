935ac1662dea16031453e474a834136074852cce
winterbokeh <2082881702@qq.com>
2021-01-24 16:02:37 +0800

    add database

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS bilibili;
+USE bilibili;
+
+DROP TABLE IF EXISTS `userinfo`;
+
+CREATE TABLE `userinfo` (
+    `uid` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(15) NOT NULL,
+    `phone` VARCHAR(11) NOT NULL, 
+    `salt` VARCHAR(10) NOT NULL,
+    `password` VARCHAR(32) NOT NULL,
+    `reg_date` DATE NOT NULL,
+    `email` VARCHAR(20), 
+    `statement` VARCHAR(30) DEFAULT '这个人很懒，什么都没有写',
+    UNIQUE (`username`),
+    UNIQUE (`phone`)
+);
