d746e5e232d251c02c0590e5d91ffb3199bd90ba
Rhys <lomirus@qq.com>
2021-01-25 21:07:28 +0800

    finished recommend column

--- view/statics/js/video.js
+++ view/statics/js/video.js
@@ -1,4 +1,5 @@
 const danmaku_switch = document.querySelector('#video>.bottom>.control>.switch')
+const recommend = document.querySelector('#recommend')
 
 danmaku_switch.addEventListener('click', () => {
     if (danmaku_switch.classList.contains('on')) {
@@ -9,3 +10,68 @@ danmaku_switch.addEventListener('click', () => {
         danmaku_switch.classList.add('on')
     }
 })
+
+function loadRecommend(data){
+    data.forEach(v => {
+        let section = document.createElement('section')
+        section.innerHTML =
+            `<div><img src="${v.cover}"></div>`+
+            `<p class="title">${v.title}</p>`+
+            `<p class="author">${v.author}</p>`+
+            `<p class="data"><span class="play_number">${v.play_number}</span>`+
+            `<span class="danmaku_number">${v.danmaku_number}</span></p>`
+        recommend.appendChild(section)
+    })
+}
+
+function test(){
+    let recommend_data = [
+        {
+            cover: '/statics/test/rcmd7.webp',
+            title: '【8848】跌跟头手机',
+            author: '古月浪子',
+            play_number: '162',
+            danmaku_number: '10',
+        },{
+            cover: '/statics/test/rcmd2.webp',
+            title: '[CS:GO]经典差点干掉队友拿五杀',
+            author: 'ほしの雲しょう',
+            play_number: '6',
+            danmaku_number: '0',
+        },{
+            cover: '/statics/test/rcmd3.webp',
+            title: 'CSS进阶',
+            author: 'kying-star',
+            play_number: '789',
+            danmaku_number: '2',
+        },{
+            cover: '/statics/test/rcmd4.webp',
+            title: 'Web后端第四节课-go杂谈&常用包',
+            author: 'sarail',
+            play_number: '48',
+            danmaku_number: '0',
+        },{
+            cover: '/statics/test/rcmd5.png',
+            title: '我是#鹿乃#的NO.008757号真爱粉，靓号在手，走路带风，解锁专属粉丝卡片，使用专属粉丝装扮，你也来生成你的专属秀起来吧！',
+            author: '辇道增柒',
+            play_number: '40',
+            danmaku_number: '0',
+        },{
+            cover: '/statics/test/rcmd6.webp',
+            title: '打爆灯塔！快乐的Sword Art Online: Fatal Bullet',
+            author: 'ほしの雲しょう',
+            play_number: '74',
+            danmaku_number: '0',
+        },{
+            cover: '/statics/test/rcmd1.webp',
+            title: 'Dota2主播日记226期：翔哥NB，zardNB，肚皇NB（都破音）',
+            author: '抽卡素材库',
+            play_number: '4183',
+            danmaku_number: '29',
+        },
+    ]
+
+    loadRecommend(recommend_data)
+}
+
+test()
