67c88e0a9b4a5a7f441e39b1795e3d033068cb80
Rhys <lomirus@qq.com>
2021-01-28 23:08:40 +0800

    [feature] 现可对弹幕字体设置面板进行操作

--- view/statics/js/video.js
+++ view/statics/js/video.js
@@ -1,21 +1,20 @@
 const danmaku_switch = document.querySelector('#video>.bottom>.control>.switch')
+const danmaku_font_settings = document.querySelector('#video>.hover>.font_settings')
+const danmaku_font_switch = document.querySelector('#video>.bottom>.control>.send>.edit>div')
+const scroll_type = document.querySelector('.font_settings>.type>.scroll')
+const top_type = document.querySelector('.font_settings>.type>.top')
+const bottom_type = document.querySelector('.font_settings>.type>.bottom')
 const recommend = document.querySelector('#recommend')
+const color_input = document.querySelector('.font_settings>.color>.edit>input')
+const color_preview = document.querySelector('.font_settings>.color>.edit>div')
+const color_list = document.querySelector('.font_settings>.color>.list').children
 
-danmaku_switch.addEventListener('click', () => {
-    if (danmaku_switch.classList.contains('on')) {
-        danmaku_switch.classList.remove('on')
-        danmaku_switch.classList.add('off')
-    } else {
-        danmaku_switch.classList.remove('off')
-        danmaku_switch.classList.add('on')
-    }
-})
 
 function loadRecommend(data){
     data.forEach(v => {
         let section = document.createElement('section')
         section.innerHTML =
-            `<div><img src="${v.cover}"></div>`+
+            `<div><img alt="${v.title}" src="${v.cover}"></div>`+
             `<p class="title">${v.title}</p>`+
             `<p class="author">${v.author}</p>`+
             `<p class="data"><span class="play_number">${v.play_number}</span>`+
@@ -24,6 +23,63 @@ function loadRecommend(data){
     })
 }
 
+function rgbToHex(rgb){
+    return '#' + rgb.slice(4, -1).split(', ').map(v => {
+        let hex = parseInt(v).toString(16)
+        return hex.length === 1 ? '0' + hex : hex
+    }).join('').toUpperCase()
+}
+function checkHex(hex){
+    if(hex.length !== 7)
+        return false
+    if (hex[0] !== '#')
+        return false
+    return hex.substring(1).every(v => '0123456789ABCDEF'.includes(v))
+}
+
+function init(){
+    danmaku_switch.addEventListener('click', () => {
+        if (danmaku_switch.classList.contains('on')) {
+            danmaku_switch.classList.remove('on')
+            danmaku_switch.classList.add('off')
+        } else {
+            danmaku_switch.classList.remove('off')
+            danmaku_switch.classList.add('on')
+        }
+    })
+    danmaku_font_switch.addEventListener('mouseover', () => danmaku_font_settings.style.display = 'block')
+    danmaku_font_switch.addEventListener('mouseout', () => danmaku_font_settings.style.display = 'none')
+    danmaku_font_settings.addEventListener('mouseover', () => danmaku_font_settings.style.display = 'block')
+    danmaku_font_settings.addEventListener('mouseout', () => danmaku_font_settings.style.display = 'none')
+    scroll_type.addEventListener('click', () => {
+        scroll_type.classList.add('chosen')
+        top_type.classList.remove('chosen')
+        bottom_type.classList.remove('chosen')
+    })
+    top_type.addEventListener('click', () => {
+        scroll_type.classList.remove('chosen')
+        top_type.classList.add('chosen')
+        bottom_type.classList.remove('chosen')
+    })
+    bottom_type.addEventListener('click', () => {
+        scroll_type.classList.remove('chosen')
+        top_type.classList.remove('chosen')
+        bottom_type.classList.add('chosen')
+    })
+    color_input.addEventListener('input', () => {
+        color_input.value = color_input.value.toUpperCase()
+        if(checkHex(color_input.value))
+            color_preview.style.backgroundColor = color_input.value
+        else
+            color_preview.style.backgroundColor = 'rgba(0,0,0,0)'
+    })
+    for (let i = 0; i < color_list.length; i++) {
+        color_list[i].addEventListener('click', () => {
+            color_input.value = rgbToHex(color_list[i].style.backgroundColor)
+            color_preview.style.backgroundColor = color_list[i].style.backgroundColor
+        })
+    }
+}
 function test(){
     let recommend_data = [
         {
@@ -68,10 +124,17 @@ function test(){
             author: '抽卡素材库',
             play_number: '4183',
             danmaku_number: '29',
-        },
+        },{
+            cover: '/statics/test/rcmd8.webp',
+            title: '【原神钢琴】晚安，璃月 | Good Night, Liyue',
+            author: 'jerritaaa',
+            play_number: '33',
+            danmaku_number: '0',
+        }
     ]
 
     loadRecommend(recommend_data)
 }
 
+init()
 test()
