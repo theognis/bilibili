f229fd1adbf0b902d2580fea4b8305b5338fb4ba
winterbokeh <2082881702@qq.com>
2021-02-03 19:40:39 +0800

    refactor：把对字段的叙述从sql文件中转移到docs中

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ CREATE TABLE `userinfo`
 (
     `uid`                INT AUTO_INCREMENT PRIMARY KEY,
     `username`           VARCHAR(15)  NOT NULL,
-    `gender`             VARCHAR(1)   NOT NULL DEFAULT 'N', --N为未知，F女性，M男性，O其它。
+    `gender`             VARCHAR(1)   NOT NULL DEFAULT 'N',
     `phone`              VARCHAR(11)  NOT NULL,
     `salt`               VARCHAR(10)  NOT NULL,
     `password`           VARCHAR(32)  NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE `userinfo`
     `statement`          VARCHAR(90)  NOT NULL DEFAULT '这个人很懒，什么都没有写',
     `avatar`             VARCHAR(120) NOT NULL DEFAULT 'https://redrock.oss-cn-chengdu.aliyuncs.com/akari.jpg',
     `reg_date`           DATE         NOT NULL,
-    `birthday`           DATE         NOT NULL DEFAULT '9999-12-12', --"Birthday": "9999-12-12T00:00:00Z"为用户未设置生日
+    `birthday`           DATE         NOT NULL DEFAULT '9999-12-12',
     `last_check_in_date` DATE         NOT NULL DEFAULT '1926-08-17',
     `exp`                INT          NOT NULL DEFAULT 0,
     `coins`              INT          NOT NULL DEFAULT 0,
