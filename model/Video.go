40c177f83e669bc8ceb8a3181fa87e1e00686d19
winterbokeh <2082881702@qq.com>
2021-02-22 10:37:40 +0800

    refactor: 修改了视频model命名规范

--- model/Video.go
+++ model/Video.go
@@ -3,17 +3,18 @@ package model
 import "time"
 
 type Video struct {
-	Av          int64
+	Id          int64
 	Title       string
 	Channel     string
 	Description string
-	VideoUrl    string
-	CoverUrl    string
-	AuthorUid   int64
+	Video       string
+	Cover       string
+	Author      int64
 	Time        time.Time
 	Views       int64
 	Likes       int64
 	Coins       int64
 	Saves       int64
 	Shares      int64
+	Length      string
 }
